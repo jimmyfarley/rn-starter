9bf08604d95f6ca16d41e5b91c332180204c4823
Jimmy <jimmyfarleydev@gmail.com>
2021-01-17 15:19:29 -0500

    screens/auth/register: add inputs

--- screens/auth/register.js
+++ screens/auth/register.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
 
 export default function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordRe, setPasswordRe] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Register</Text>
+      <Text style={styles.logo}>App Name</Text>
+      <View style={styles.inputView} >
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email..."
+          placeholderTextColor="#003f5c"
+          value={email}
+          onChangeText={setEmail} />
+      </View>
+      <View style={styles.inputView} >
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          value={password}
+          placeholder="Password..."
+          placeholderTextColor="#003f5c"
+          onChangeText={setPassword} />
+      </View>
+      <View style={styles.inputView} >
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          value={passwordRe}
+          placeholder="Confirm Password..."
+          placeholderTextColor="#003f5c"
+          onChangeText={setPasswordRe} />
+      </View>
+      <TouchableOpacity>
+        <Text style={styles.forgot}>Already have an account?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.loginText}>Signup</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,8 +48,44 @@ export default function Register() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#003f5c',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#fb5b5a",
+    marginBottom: 40
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#465881",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
+  },
+  inputText: {
+    height: 50,
+    color: "white"
+  },
+  forgot: {
+    color: "white",
+    fontSize: 11
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10
+  },
+  loginText: {
+    color: "white"
+  }
 });
